0ed559b55baed7493dd1350e654f3d74bcc41069
Aline Marinho <alinemarinho.am13@gmail.com>
2022-04-14 22:08:17 -0300

    projeto APOD

---
+++ api.js
@@ -0,0 +1,32 @@
+const date = $('#date')
+const chaveapi = "EFixur4uqzTmJzfRukfYS2B1lwwfnJmL6yZ5gfyg";
+
+
+
+$("#botao").click(function() {
+    $.ajax({
+    url:`https://api.nasa.gov/planetary/apod?api_key=${chaveapi}&date=${date.val()}`,
+    method: "GET",
+
+    success:function(resposta){
+      $("#resposta").html(function(){
+      if(resposta.media_type ==="image"){
+
+        return `<img class="imagem"src="${resposta.url}"/>`
+      }else{
+
+        return `<iframe class="video" src="${resposta.url}" frameborder="0"></iframe>`;
+      }
+      })  
+    },
+    error:function(error){
+        console.error(error)
+    }
+
+    })
+} )
+
+
+$("form").submit(function(event) {
+    event.preventDefault()
+} )
